cfaad8059ee0879d112a1fad981fc04dc2baac2f
Admin <admin@example.com>
2025-12-23 11:53:45 +0800

    Update for server deployment (include uploads, db, dist)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-90b86364'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.78o0nu2slsc"
+    "revision": "0.l4gc1nb5lio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,10 +1,18 @@
 module.exports = {
   apps: [{
-    name: "zju-platform",
+    name: "tuotu-zhexiang",
     script: "./server/index.js",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
     env: {
       NODE_ENV: "production",
-      PORT: 80
+      PORT: 3001
+    },
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 3001
     }
   }]
-}
+};
