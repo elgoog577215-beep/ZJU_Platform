35609cb64c82fbf20eff5f6a208ee54bc64cb46e
Nothing-cell-bit <565949468@qq.com>
2026-07-05 16:54:29 +0800

    fix: add webview load fallback

--- wechat-miniprogram/pages/webview/index.js
+++ wechat-miniprogram/pages/webview/index.js
@@ -5,14 +5,37 @@ Page({
     src: "",
   },
 
+  loadTimer: null,
+
   onLoad(options = {}) {
     const targetPath = options.path || options.url || DEFAULT_PATH;
     this.setData({
       src: buildWebViewUrl(targetPath),
     });
+
+    this.loadTimer = setTimeout(() => {
+      wx.redirectTo({
+        url: "/pages/fallback/index?reason=timeout",
+      });
+    }, 8000);
+  },
+
+  onUnload() {
+    this.clearLoadTimer();
+  },
+
+  clearLoadTimer() {
+    if (!this.loadTimer) return;
+    clearTimeout(this.loadTimer);
+    this.loadTimer = null;
+  },
+
+  handleLoad() {
+    this.clearLoadTimer();
   },
 
   handleError() {
+    this.clearLoadTimer();
     wx.redirectTo({
       url: "/pages/fallback/index?reason=load",
     });
